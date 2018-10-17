58536af1a32a7faa7b090dec5b1a0175ba480f9d
Haaxor1689 <betkomaros@gmail.com>
2018-10-17 23:59:02 +0200

    Added documentation.

--- exception.hpp
+++ exception.hpp
@@ -3,17 +3,21 @@
 #include <string>
 #include <exception>
 
+/// Default Patlib namespace
 namespace ptl {
 
+/// Patlib's exception class
 class exception : public std::exception {
     std::string message;
 
 public:
     exception(const std::string& message) : message(message) {}
 
+    /// Returns explanatory string.
+    /// @return Pointer to a null-terminated string with explanatory information.
     const char* what() const override {
         return message.c_str();
     }
 };
 
-}
+} // namespace ptl
