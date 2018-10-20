a24bee30b5c0e0f5845ab5280643c023d56ac311
Haaxor1689 <betkomaros@gmail.com>
2018-10-20 17:09:36 +0200

    Completed rough project refactoring.

--- opatgen.cpp
+++ opatgen.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <vector>
 #include <string>
-#include <fstream>
 
 #include "candidate_count_trie.hpp"
 #include "exception.hpp"
@@ -20,10 +18,8 @@
 const char* opatgen_version = "1.0";
 const char* opatgen_cvs_id = "$Id: opatgen.w,v 1.24 2001/12/03 17:51:13 antos Exp $";
 
-using namespace std;
 using namespace ptl;
 
-bool utf_8;
 
 typedef unsigned long Tindex;
 typedef unsigned Tin_alph;
@@ -64,41 +60,42 @@ typedef level<Tindex, Tin_alph, Tval_type, Twt_type,
               Tcount_type, THword, TTranslate, TCandidate_count_structure,
               TCompetitive_multi_out_pat_manip, TWord_input_file, TPass> TLevel;
 
-void print_banner(void) {
-    cout << endl;
-    cout << "Written and maintained by David Antos, xantos (at) fi.muni.cz" << endl;
-    cout << "Copyright (C) 2001 David Antos" << endl;
-    cout << "This is free software; see the source for copying ";
-    cout << "conditions. There is NO" << endl;
-    cout << "warranty; not even for MERCHANTABILITY or FITNESS ";
-    cout << "FOR A PARTICULAR PURPOSE." << endl << endl;
-    cout << "Thank you for using free software!" << endl << endl;
+void print_banner() {
+    std::cout << std::endl;
+    std::cout << "Written and maintained by David Antos, xantos (at) fi.muni.cz" << std::endl;
+    std::cout << "Copyright (C) 2001 David Antos" << std::endl;
+    std::cout << "This is free software; see the source for copying ";
+    std::cout << "conditions. There is NO" << std::endl;
+    std::cout << "warranty; not even for MERCHANTABILITY or FITNESS ";
+    std::cout << "FOR A PARTICULAR PURPOSE." << std::endl << std::endl;
+    std::cout << "Thank you for using free software!" << std::endl << std::endl;
 }
 
 int main(int argc, char* argv[]) {
-    cout << "This is OPATGEN, version " << opatgen_version << endl;
+    std::cout << "This is OPATGEN, version " << opatgen_version << std::endl;
 
     if (argc >= 2 && (0 == strcmp(argv[1], "--help"))) {
-        cout << "Usage: opatgen [-u8] DICTIONARY PATTERNS OUTPUT TRANSLATE" << endl;
-        cout << "  Generate the OUTPUT hyphenation file from the" << endl;
-        cout << "  DICTIONARY, PATTERNS, and TRANSLATE files." << endl << endl;
-        cout << "  -u8     files are in UTF-8 UNICODE encoding." << endl << endl;
-        cout << "opatgen --help     print this help" << endl;
-        cout << "opatgen --version  print version information" << endl;
+        std::cout << "Usage: opatgen [-u8] DICTIONARY PATTERNS OUTPUT TRANSLATE" << std::endl;
+        std::cout << "  Generate the OUTPUT hyphenation file from the" << std::endl;
+        std::cout << "  DICTIONARY, PATTERNS, and TRANSLATE files." << std::endl << std::endl;
+        std::cout << "  -u8     files are in UTF-8 UNICODE encoding." << std::endl << std::endl;
+        std::cout << "opatgen --help     print this help" << std::endl;
+        std::cout << "opatgen --version  print version information" << std::endl;
         print_banner();
         return 0;
     }
 
     if (argc >= 2 && (0 == strcmp(argv[1], "--version"))) {
-        cout << "(CVS: " << opatgen_cvs_id << ")" << endl;
-        cout << "with PATLIB, version " << patlib_version << endl;
-        cout << "(CVS: " << patlib_cvs_id << ")" << endl;
+        std::cout << "(CVS: " << opatgen_cvs_id << ")" << std::endl;
+        std::cout << "with PATLIB, version " << patlib_version << std::endl;
+        std::cout << "(CVS: " << patlib_cvs_id << ")" << std::endl;
         print_banner();
         return 0;
     }
 
     print_banner();
-
+    
+    bool utf_8;
     try {
         if (argc == 5) {
             utf_8 = false;
@@ -122,26 +119,19 @@ int main(int argc, char* argv[]) {
                     g(argv[2], argv[3], argv[4], argv[5]);
             g.do_all();
         } else {
-            cout << "opatgen: needs some arguments" << endl
-                    << "Try `opatgen --help'" << endl;
+            std::cout << "opatgen: needs some arguments" << std::endl
+                      << "Try `opatgen --help'" << std::endl;
             return 1;
         }
     } catch (const ptl::exception& e) {
-        cerr << e.what() << endl;
-        cerr << endl << "This was fatal error, sorry. Giving up." << endl;
+        std::cerr << e.what() << std::endl;
+        std::cerr << std::endl << "This was fatal error, sorry. Giving up." << std::endl;
+        return -1;
+    } catch (...) {
+        std::cerr << "An unexpected exception occurred. It means there is probably" << std::endl;
+        std::cerr << "a bug in the program. Please report it to the maintainer." << std::endl;
+        std::cerr << "Use opatgen --version to find out who the maintainer is.";
+        return -1;
     }
-    catch (...) {
-        cerr << "An unexpected exception occurred. It means there is probably" << endl;
-        cerr << "a bug in the program. Please report it to the maintainer." << endl;
-        cerr << "Use opatgen --version to find out who the maintainer is.";
-        cout << "Do you want me to dump core? <y/n> " << endl;
-        string s;
-        cin >> s;
-        if (s == "y" || s == "Y") {
-            cout << endl << "Now I dump core..." << endl;
-            terminate();
-        }
-    }
+    return 0;
 }
-
-/*:61*/
